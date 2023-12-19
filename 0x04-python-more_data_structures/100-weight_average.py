72cb19a970b2595b1c14248c1fb1a5b61ebe54cd
Ysn-elm089 <yassine.elmahdaoui89@gmail.com>
2023-12-18 22:43:40 -0800

    error fixed

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,4 +2,11 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    else (sum(a * b for a, b in my_list) / sum(b for a, b in my_list))
+
+    num = 0
+    den = 0
+
+    for tup in my_list:
+        num += tup[0] * tup[1]
+        den += tup[1]
+        return (num / den)
