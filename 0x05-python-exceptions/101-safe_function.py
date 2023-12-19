78180735d3a38f1045891612fa6793c37969fb39
Ysn-elm089 <yassine.elmahdaoui89@gmail.com>
2023-12-18 20:40:08 -0800

    flash

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,8 +7,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return (result)
-    except:
+    except Exception as i:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
-    else
-    return (result)
