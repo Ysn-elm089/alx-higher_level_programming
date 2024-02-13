be4ce87d7ee1350dd198277576ca201e763428b6
Ysn-elm089 <yassine.elmahdaoui89@gmail.com>
2024-02-13 10:53:56 -0800

    strict

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
--- Lists all records of the table second_table having a name value in my MySQL server.
--- Records are ordered by descending score.
-SELECT `score`, `name`
+-- Lists the number of records with the same score in the table second_table in my MySQL server.
+-- Records are ordered by descending count.
+SELECT `score`, COUNT(*) AS `number`
 FROM `second_table`
-WHERE `name` != ""
-ORDER BY `score` DESC;
+GROUP BY `score`
+ORDER BY `number` DESC;
