f585e353eed34d1f688816d7e300bf7b6c78216e
Ysn-elm089 <yassine.elmahdaoui89@gmail.com>
2024-03-29 16:11:13 -0700

    fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,24 +1,13 @@
-
 #!/usr/bin/python3
 """
 Displays the value of the X-Request-Id variable
-found in the header of the response
+found in the header of the response.
 """
-import urllib.request
-from sys import argv
 
-
-def main(argv):
-    """
-    Method that takes in a URL, sends a request to the URL
-    and displays the value of the X-Request-Id variable
-    found in the header of the response
-    """
-    url = argv
-    with urllib.request.urlopen(url) as response:
-        headers = response.info()
-        print(headers['X-Request-Id'])
 
 if __name__ == "__main__":
-    main(argv[1])
+    import sys
+    import urllib.request
 
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers["X-Request-Id"])
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
 """
-Displays the value of the X-Request-Id variable
-found in the header of the response
-"""
-import requests
-from sys import argv
+Sends a request to the URL and displays the value of the variable X-Request-Id
+in the response header"""
 
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
 
-def main(argv):
-    """
-    Method that takes in a URL, sends a request to the URL
-    and displays the value of the X-Request-Id variable
-    found in the header of the response
-    """
-    url = argv[1]
-    r = requests.get(url)
-    headers = r.headers.get('X-Request-Id')
-    print(headers)
-
-if __name__ == "__main__":
-    main(argv)
+    res = get(argv[1])
+    print(res.headers.get('X-Request-Id'))
