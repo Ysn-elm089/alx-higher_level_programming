6d9db5350374e841cfafe19f16b8016871d70056
Ysn-elm089 <yassine.elmahdaoui89@gmail.com>
2024-04-24 12:12:02 -0700

    rosa

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-// Import the built-in Node.js 'fs' module.
 
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  // Use fs.readFile() to read the contents of a file specified as a command-line argument
-  // 'utf8' specifies the encoding of the file being read
+const filePath = process.argv[2];
 
-  if (error) {
-    // If an error occurs during the file read operation, the 'error' parameter will contain an error object.
-    console.error('Error reading the file:', error);
+if (!filePath) {
+    console.error('Usage: node 0-readme.js <file_path>');
+    process.exit(1);
+}
 
-  } else {
-    // If the file is read successfully, the 'content' parameter will contain the contents of the file as a string.
-    console.log(content);
-  }
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(data);
 });
 
