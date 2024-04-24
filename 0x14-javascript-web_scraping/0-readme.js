54781e3d6a429ad97882d9d9352563fff096ed1f
Ysn-elm089 <yassine.elmahdaoui89@gmail.com>
2024-04-24 12:18:28 -0700

    rosa

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,16 +3,11 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 
-if (!filePath) {
-    console.error('Usage: node 0-readme.js <file_path>');
-    process.exit(1);
-}
-
 fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
 
